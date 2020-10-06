218de2050c6d5ca77f71748a055272b9597b9e2d
Victor <victor.develops@gmail.com>
2020-10-06 15:52:19 +0200

    angular 8 downgrade =(

--- src/app/content/catalog/product.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-  name: string = "Eclipse(Demo)";
-  description: string = "Lorem ipsum semper fidelis lorem ipsum lorem ipsum.";
-  price: number = 20;
-  link: string ="";
-  imgLink: string ="https://cdn.pixabay.com/photo/2016/02/24/18/37/fog-1220491_960_720.jpg";
-  type: string = "CD";
-}
