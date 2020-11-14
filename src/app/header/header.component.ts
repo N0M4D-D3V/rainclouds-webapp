0df5ecf2a311b5dcdce7c6815f631329cd84e053
Victor <victor.develops@gmail.com>
2020-11-14 08:45:32 +0100

    phone modal styles fixed // header links added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,6 +4,7 @@ import { ArtModalComponent } from "../modals/art-modal/art-modal.component";
 import { ContactModalComponent } from "../modals/contact-modal/contact-modal.component";
 import { MusicModalComponent } from "../modals/music-modal/music-modal.component";
 import { ProductionComponent } from "../modals/production/production.component";
+import { GlobalConstants } from "../common/global-constants";
 
 @Component({
   selector: "app-header",
@@ -11,6 +12,13 @@ import { ProductionComponent } from "../modals/production/production.component";
   styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  //RAZAK
+  youtube: string = GlobalConstants.YOUTUBE_URL;
+  bandcamp: string = GlobalConstants.BANDCAMP_URL;
+  instagram: string = GlobalConstants.INSTAGRAM_URL;
+  facebook: string = GlobalConstants.FACEBOOK_URL;
+  spotify: string = GlobalConstants.SPOTIFY_URL;
+
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
