908cc42a009378e9c43f4df7d544512125b31442
SnowCrash <victor.develops@gmail.com>
2021-03-24 19:37:41 +0100

    lyric service added | some fixs

--- src/app/common/utils.ts
+++ src/app/common/utils.ts
@@ -1,6 +1,7 @@
-import { Carousel } from "../interfaces/carousel";
-import { Product } from "../interfaces/product";
-import { Image } from "../interfaces/image";
+import { Carousel } from '../interfaces/carousel';
+import { Product } from '../interfaces/product';
+import { Image } from '../interfaces/image';
+import { Lyric, Paragraph } from '../interfaces/lyric';
 
 export function renderProduct(item: any): Product {
   const product = item.payload.doc.data() as Product;
@@ -18,6 +19,21 @@ export function renderCarousel(item: any): Carousel {
   const carousel = item.payload.doc.data() as Carousel;
   carousel.id = item.payload.doc.id;
   return carousel;
+}
+
+export function renderLyric(item: any): Lyric {
+  let paragraphs = item.payload.doc.data().paragraphs.split('---');
+
+  const lyric: Lyric = item.payload.doc.data() as Lyric;
+  lyric.paragraphs = [];
+  lyric.id = item.payload.doc.id;
+
+  paragraphs.forEach((paragraph) => {
+    let paragraphRendered: Paragraph = paragraph.split('_');
+    lyric.paragraphs.push(paragraphRendered);
+  });
+
+  return lyric;
 }
 
 export function shuffle(array: Array<any>): Array<any> {
--- src/app/components/content/lyrics/lyrics.component.ts
+++ src/app/components/content/lyrics/lyrics.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { BannerImages } from 'src/app/common/global-constants';
+import { Lyric } from 'src/app/interfaces/lyric';
+import { LyricService } from '../../../services/lyric.service';
 
 @Component({
   selector: 'app-lyrics',
   templateUrl: './lyrics.component.html',
-  styleUrls: ['./lyrics.component.css']
+  styleUrls: ['./lyrics.component.scss'],
 })
-export class LyricsComponent implements OnInit {
+export class LyricsComponent implements OnInit, AfterViewInit {
+  public bannerImg: string = BannerImages.BANNER_IMG_CATALOG;
+  public type: string = 'LYRICS';
 
-  constructor() { }
+  public lyrics: Lyric[] = new Array<Lyric>();
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private lyricService: LyricService
+  ) {}
 
   ngOnInit(): void {
+    this.lyricService
+      .getAllLyrics()
+      .subscribe((response) => (this.lyrics = response));
   }
 
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
 }
