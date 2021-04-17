689a55123351b15fce4c76ea9921265de947b1fe
SnowCrash <victor.develops@gmail.com>
2021-04-17 18:08:14 +0200

    SEO update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,14 +6,16 @@ import { Meta, Title } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private titleService: Title, private metaService: Meta) {
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
     this.titleService.setTitle('RazaK BlacK MetaL');
     this.metaService.addTags([
       {
         name: 'keywords',
         content:
-          'Razak, Black Metal, Post Black Metal, Atmospheric, Eclipse, Rainclouds, Necrolove, Cradle of Filth, Burzum, Lustre',
+          'Razak, Black Metal, Post Black Metal, Atmospheric, Eclipse, Rainclouds, Necrolove, Cradle of Filth, Burzum, Lustre, Music',
       },
       {
         name: 'description',
@@ -21,6 +23,8 @@ export class AppComponent {
           "Razak Black Metal. An Atmospheric/Post Black Metal band from Spain. Listen to RazaK music and contact with us! IT'S NOT ABOUT DEATH. IT'S ABOUT SUFFERING.",
       },
       { name: 'robots', content: 'index, follow' },
+      { charset: 'UTF-8' },
+      { name: 'author', content: 'RazaK BlacK MetaL' },
     ]);
   }
 }
