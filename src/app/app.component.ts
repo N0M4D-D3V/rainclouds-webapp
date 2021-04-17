68bddd54258fe8c2456872e9977676756dc533c1
SnowCrash <victor.develops@gmail.com>
2021-04-17 17:59:18 +0200

    SEO || Alt || tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Razak - DSBM/Post Black Metal Band';
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle('RazaK BlacK MetaL');
+    this.metaService.addTags([
+      {
+        name: 'keywords',
+        content:
+          'Razak, Black Metal, Post Black Metal, Atmospheric, Eclipse, Rainclouds, Necrolove, Cradle of Filth, Burzum, Lustre',
+      },
+      {
+        name: 'description',
+        content:
+          "Razak Black Metal. An Atmospheric/Post Black Metal band from Spain. Listen to RazaK music and contact with us! IT'S NOT ABOUT DEATH. IT'S ABOUT SUFFERING.",
+      },
+      { name: 'robots', content: 'index, follow' },
+    ]);
+  }
 }
